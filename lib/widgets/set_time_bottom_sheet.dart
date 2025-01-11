9c78b977addd82d14e3d7fc92a912df66ec1b5a1
Adityaverma1998 <vermaaditya860@gmail.com.com>
2025-01-11 09:18:45 +0530

    fix: fixes bottomsheet typo

--- lib/widgets/set_time_bottom_sheet.dart
+++ lib/widgets/set_time_bottom_sheet.dart
@@ -2,18 +2,17 @@ import 'package:flutter/material.dart';
 
 void showTimePickerBottomSheet(BuildContext context) {
   List<int> minValues = List.generate(61, (index) => index);
-  List<int> secValues = List.generate(13, (index) => index * 5); 
+  List<int> secValues = List.generate(13, (index) => index * 5);
 
   double itemHeight = 30;
   var selectedMin = minValues[(minValues.length / 2).toInt()];
   var selectedSec = secValues[(secValues.length / 2).toInt()];
 
- 
-
   showModalBottomSheet(
     context: context,
     builder: (BuildContext context) {
       return Container(
+        width: MediaQuery.of(context).size.width ,
         padding: EdgeInsets.all(16.0),
         alignment: Alignment.center,
         child: Column(
@@ -21,16 +20,46 @@ void showTimePickerBottomSheet(BuildContext context) {
           children: <Widget>[
             Text(
               'Set Time',
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
+            Row(mainAxisAlignment: MainAxisAlignment.center, children: <Widget>[
+              Container(
+                                color:Colors.blue,
 
-                
-                     
-              
-              ])    
+                  height: MediaQuery.of(context).size.height * 0.4,
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  child: ListView.builder(
+                    physics: const ClampingScrollPhysics(),
+                    itemCount: minValues.length,
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        title: Text('${minValues[index]}'),
+                        titleTextStyle: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500
+                        ),
+                      );
+                    },
+                  )),
+              Container(
+                color:Colors.red,
+                  height: MediaQuery.of(context).size.height * 0.4,
+                  width: MediaQuery.of(context).size.width * 0.4,
+                  child: Center(
+                    child: ListView.builder(
+                      physics: const ClampingScrollPhysics(),
+                      itemCount: secValues.length,
+                      itemBuilder: (context, index) {
+                        return ListTile(
+                          onTap: (){
+
+                          },
+                          title: Text('${secValues[index]}'),
+                        );
+                      },
+                    ),
+                  ))
+            ])
           ],
         ),
       );
