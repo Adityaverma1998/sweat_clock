965188aae36ebc5118a8bb9d9eba90799abc22c8
Adityaverma1998 <vermaaditya860@gmail.com.com>
2025-01-13 11:53:37 +0530

    add: add update whenever app have new  version then show pop

--- lib/my_app.dart
+++ lib/my_app.dart
@@ -2,23 +2,29 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:stop_watch/providers/home.dart';
 import 'package:stop_watch/screen/home_screen.dart';
+import 'package:upgrader/upgrader.dart';
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
-                ChangeNotifierProvider(create: (_) => Home()),
-
+        ChangeNotifierProvider(create: (_) => Home()),
       ],
-      child: MaterialApp(
+      child: UpgradeAlert(
+        child: const MaterialApp(
+          title: 'SweatClock',
+          home: HomeScreen(),
+        ),
+        // showReleaseNotes: ,
+        barrierDismissible:false ,
+        dialogStyle: UpgradeDialogStyle.cupertino, 
+        showReleaseNotes: true,
         
-        title:'SweatClock',
-        home: HomeScreen(),
+  
       ),
     );
   }
-  
 }
